32aadd94cbf50ccc0330b3135b0ecd0d9ffb6e71
ivansimplicio <ivansimplicio@outlook.com>
2021-03-15 17:19:14 -0300

    Exibindo as Categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: 'http://api-virtual-store.herokuapp.com'
+    baseUrl: 'http://api-virtual-store.herokuapp.com',
+    bucketBaseUrl: 'https://bucket-s3-api-virtual-store.s3-sa-east-1.amazonaws.com'
 }
