9151f4cea3d5186b239186395ebf33905fc430d6
ivansimplicio <ivansimplicio@outlook.com>
2021-03-18 11:25:21 -0300

    Carregando produtos de uma dada categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -29,7 +29,7 @@ export class CategoriasPage {
       error => {});
   }
 
-  showProdutos(){
-    this.navCtrl.push('ProdutosPage');
+  showProdutos(categoriaId){
+    this.navCtrl.push('ProdutosPage', {categoria_id : categoriaId});
   }
 }
