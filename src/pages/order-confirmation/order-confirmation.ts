a526c19b67c284e7ec1a1db92bf791179364ae55
ivansimplicio <ivansimplicio@outlook.com>
2021-03-23 14:39:22 -0300

    Exibindo informações do pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems : CartItem[];
   cliente : ClienteDTO;
   endereco : EnderecoDTO;
+  codPedido : string;
 
   constructor(
     public navCtrl: NavController,
@@ -52,11 +53,19 @@ export class OrderConfirmationPage {
     return this.cartService.total();
   }
 
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
-        console.log(response.headers.get('location'));
         this.cartService.createOrClearCart();
+        this.codPedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if(error.status == 403){
@@ -65,7 +74,8 @@ export class OrderConfirmationPage {
       });
   }
 
-  back(){
-    this.navCtrl.setRoot('CartPage');
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position+1, location.length);
   }
 }
