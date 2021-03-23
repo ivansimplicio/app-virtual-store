3696cf5c2f29c6970dfa428968628c744e3beed7
ivansimplicio <ivansimplicio@outlook.com>
2021-03-23 14:12:29 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.module.ts
+++ src/pages/order-confirmation/order-confirmation.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { PedidoService } from '../../services/domain/pedido.service';
 import { OrderConfirmationPage } from './order-confirmation';
 
 @NgModule({
+  providers: [
+    PedidoService
+  ],
   declarations: [
     OrderConfirmationPage,
   ],
